3d3696912b7c20a7cb28f9677559e17d78ec02e5
kelignoel2 <kelignoel32@gmail.com>
2019-05-29 13:53:13 +0200

    ajout des module accueil articles equipe etudes nav footer et routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { EtudesComponent } from './etudes/etudes.component';
+import { FooterComponent } from './footer/footer.component';
 
+const appRoutes: Routes = [
+  {path: 'Equipe', component:EquipeComponent},
+  {path : 'Articles', component:ArticlesComponent},
+  {path: '', component:AccueilComponent},
+  {path: 'Etudes', component:EtudesComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArticlesComponent,
+    EquipeComponent,
+    AccueilComponent,
+    EtudesComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
